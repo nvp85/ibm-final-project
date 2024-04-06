bbf174876f22d5397ded0c90eb09a12c2cddb73d
nvp85 <nvp@zhukov.al>
2024-04-06 02:07:29 -0400

    add react router, create the landing page component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_page';
+import  Layout from './Components/Landing_Page/Landing_page';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
+        <BrowserRouter>
+          <Navbar/>
+              <Routes>
+                <Route path="/" element={<Landing_Page/>}/>
+              </Routes>
+            
+        </BrowserRouter>
     </div>
   );
 }
