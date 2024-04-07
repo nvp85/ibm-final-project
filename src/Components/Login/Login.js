81cae3630aa93855595fc7aa40d6c33fc48e973d
nvp85 <nvp@zhukov.al>
2024-04-07 02:16:41 -0400

    modify the login component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
+import { API_URL } from '../../config';
 
 
 export default function Login() {
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState('');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      if (sessionStorage.getItem("auth-token")) {
+        navigate("/")
+      }
+    }, []);
+
+    const login = async (e) => {
+      e.preventDefault();
+
+      const res = await fetch(`${API_URL}/api/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email:email,
+          password: password,
+        }),
+      });
+
+      const json = await res.json();
+
+      if (json.authtoken) {
+        sessionStorage.setItem('auth-token', json.authtoken);
+    
+        sessionStorage.setItem('email', email);
+        navigate('/');
+        window.location.reload()
+      } else {
+        if (json.errors) {
+          for (const error of json.errors) {
+            alert(error.msg);
+          }
+        } else {
+          alert(json.error);
+        }
+      }
+    };
+
     return (
         <div className="container">
         <div className="login-text">
@@ -13,10 +58,19 @@ export default function Login() {
           Are you a new member? <span><a href="/Sign_Up" style={{color: "#2190FF"}}> Sign Up Here</a></span>
         </div>
         <br />
-        <form className="login-form">
+        <form className="login-form" onSubmit={login}>
           <div className="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+            <label for="email">Email</label>
+            <input 
+                type="email" 
+                name="email" 
+                id="email" 
+                className="form-control" 
+                placeholder="Enter your email" 
+                aria-describedby="helpId" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
           </div>
           <div className="form-group">
             <label for="password" >Password</label>
@@ -27,6 +81,8 @@ export default function Login() {
               className="form-control"
               placeholder="Enter your password"
               aria-describedby="helpId"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
   
