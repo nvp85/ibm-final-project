adcc05c039c5c5113de0144dd3906627b4632741
nvp85 <nvp@zhukov.al>
2024-04-22 23:31:04 -0400

    add date and time fields into the appt form

--- src/Components/AppointmentForm/AppointmentForm.js
+++ src/Components/AppointmentForm/AppointmentForm.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { addDays, setHours, setMinutes } from "date-fns";
 
 const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
     const [name, setName] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [selectedSlot, setSelectedSlot] = useState(null);
+    const [date, setDate] = useState(setHours(setMinutes(addDays(new Date(),1),0), 8));
   
     const handleSlotSelection = (slot) => {
       setSelectedSlot(slot);
@@ -11,10 +15,12 @@ const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
   
     const handleFormSubmit = (e) => {
       e.preventDefault();
-      onSubmit({ name, phoneNumber });
+      onSubmit({ name, phoneNumber, date });
       setName('');
       setPhoneNumber('');
+      setDate(setMinutes(addDays(new Date(),1),0), 8);
     };
+
   
     return (
       <form onSubmit={handleFormSubmit} className="appointment-form">
@@ -37,6 +43,33 @@ const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
             onChange={(e) => setPhoneNumber(e.target.value)}
             required
           />
+        </div>
+        <div className='form-group'>
+            <label htmlFor="date">Date of Appointment:</label>
+            <DatePicker 
+                selected={date} 
+                onChange={(date) => setDate(date)}
+                id="date"
+                placeholderText="Pick a date"
+                minDate={addDays(new Date(), 1)}
+                showIcon 
+            />
+        </div>
+
+        <div className='form-group'>
+            <label htmlFor="time">Book Time Slot:</label>
+            <DatePicker
+                selected={date}
+                onChange={(date) => setDate(date)}
+                showTimeSelect
+                showTimeSelectOnly
+                timeIntervals={15}
+                timeCaption="Time"
+                dateFormat="h:mm aa"
+                id='time'
+                minTime={setHours(setMinutes(new Date(), 45), 7)}
+                maxTime={setHours(setMinutes(new Date(), 0), 18)}
+                />
         </div>
         <button type="submit">Book Now</button>
       </form>
