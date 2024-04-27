89912954982e58858d438999f684733bbe951bc1
nvp85 <nvp@zhukov.al>
2024-04-26 23:45:39 -0400

    modify notification.js and notification.css

--- src/Components/Notification/Notification.js
+++ src/Components/Notification/Notification.js
@@ -1,41 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../Navbar/Navbar';
+import './Notification.css'
 
 const Notification = ({ children }) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
     const [username, setUsername] = useState("");
-    const [doctorData, setDoctorData] = useState(null);
-    const [appointmentData, setAppointmentData] = useState(null);
+    //const [doctorData, setDoctorData] = useState(null);
+    const [appointmentsData, setAppointmentsData] = useState([]);
     useEffect(() => {
         const storedUsername = sessionStorage.getItem('email');
-        const storedDoctorData = JSON.parse(localStorage.getItem('doctorData'));
-        const storedAppointmentData = JSON.parse(localStorage.getItem(storedDoctorData?.name));
+        //const storedDoctorData = JSON.parse(localStorage.getItem('apptsData'));
+        const storedAppointmentsData = JSON.parse(localStorage.getItem('apptsData'));
+
         if (storedUsername) {
         setIsLoggedIn(true);
         setUsername(storedUsername);
         }
-        if (storedDoctorData) {
+        /* if (storedDoctorData) {
         setDoctorData(storedDoctorData);
+        } */
+        if (storedAppointmentsData) {
+            let apptsDataArray = [];
+            for (let doctor in storedAppointmentsData) {
+                apptsDataArray.push(...storedAppointmentsData[doctor]);
+            }
+            setAppointmentsData(apptsDataArray);
         }
-        if (storedAppointmentData) {
-        setAppointmentData(storedAppointmentData);
-        }
-  }, []);
+  }, [localStorage.getItem('apptsData')]);
     return (
         <div>
         <Navbar ></Navbar>
         {children}
-        {isLoggedIn && appointmentData && (
-            <>
+        {isLoggedIn && appointmentsData && (
+            
             <div className="appointment-card">
-                <div className="appointment-card__content">
-                <h3 className="appointment-card__title">Appointment Details</h3>
-                <p className="appointment-card__message">
-                    <strong>Doctor:</strong> {doctorData?.name}
-                </p>
-                </div>
+                {
+                    appointmentsData.map(appt => (
+                        <div className="appointment-card__content" key={appt.id}>
+                            <h3 className="appointment-card__title">Appointment Details</h3>
+                            <ul className="appointment-card__message">
+                                <li><strong>Doctor:</strong> {appt.doctor}</li>
+                                <li><strong>Speciality:</strong> {appt.speciality}</li>
+                                <li><strong>Name:</strong> {appt.name}</li>
+                                <li><strong>Phone Number:</strong> {appt.phoneNumber}</li>
+                                <li><strong>Date:</strong> {appt.date}</li>
+                                <li><strong>Time:</strong> {appt.time}</li>
+                            </ul>
+                        </div> 
+                    ))
+                }
             </div>
-            </>
+        
         )}
         </div>
     );
