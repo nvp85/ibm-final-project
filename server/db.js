90725adef485ca0178ef85c6d19b35dd9d7da855
nvp85 <nvp85@zhukov.al>
2024-05-04 00:05:13 -0400

    modify ProfileCard.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MTk5My1wYXJhYm9s@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:Mjc1MS1wYXJhYm9s@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/Components/Sign_Up/Sign_Up.js
+++ src/Components/Sign_Up/Sign_Up.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Sign_Up.css";
 import { API_URL } from '../../config'
 
